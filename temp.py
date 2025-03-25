339f45436183cbe0f383f8e1b30a486151e66616
AAleka <mr.allnur.55@gmail.com>
2025-03-25 11:42:03 +0300

    add data

--- temp.py
+++ temp.py
@@ -1,24 +1,65 @@
 import torch
-import torch.nn.functional as F
 import networkx as nx
+import torch.nn.functional as F
 
-from torch import nn
-from torch_geometric.nn import GINConv, global_add_pool
+from tqdm import tqdm
+from torch_geometric.nn import GATConv, GINConv, GAE
 from torch_geometric.data import Data
 from torch_geometric.loader import DataLoader
     
 
+class GINEncoder(torch.nn.Module):
+    def __init__(self, in_channels, hidden_dim, out_channels):
+        super().__init__()
+        mlp1 = torch.nn.Sequential(
+            torch.nn.Linear(in_channels, hidden_dim), 
+            torch.nn.ReLU(), 
+            torch.nn.Linear(hidden_dim, hidden_dim))
+
+        mlp2 = torch.nn.Sequential(
+            torch.nn.Linear(hidden_dim, out_channels), 
+            torch.nn.ReLU())
+
+        self.conv1 = GINConv(mlp1)
+        self.conv2 = GINConv(mlp2)
+
+    def forward(self, x, edge_index):
+        x = self.conv1(x, edge_index)
+        x = F.elu(x)
+        x = self.conv2(x, edge_index)
+        return x
+
+class GATEncoder(torch.nn.Module):
+    def __init__(self, in_channels, hidden_dim, out_channels, heads=4):
+        super().__init__()
+        self.conv1 = GATConv(in_channels, hidden_dim, heads=heads, concat=True)
+        self.conv2 = GATConv(hidden_dim * heads, out_channels, heads=1, concat=False)
+
+    def forward(self, x, edge_index):
+        x = self.conv1(x, edge_index)
+        x = F.elu(x)
+        x = self.conv2(x, edge_index)
+
+        return x
+
+class BinPackingGAE(GAE):
+    def __init__(self, in_channels, hidden_dim, out_channels):
+        encoder = GATEncoder(in_channels, hidden_dim, out_channels)
+        super().__init__(encoder)
+
+
 class BinPackingGraph:
-    def __init__(self, items, container_width):
+    def __init__(self, items, container_width, container_height):
         self.packing = []
         self.container_width = container_width
+        self.container_height = container_height
         
         self.G_x = nx.Graph()
         self.G_y = nx.Graph()
 
         self.bottom_left_packing(items)
         self.construct_graphs()
-        
+    
     def bottom_left_packing(self, items):
         is_packable = False
         for i, (w1, _) in enumerate(items):
@@ -44,29 +85,30 @@ class BinPackingGraph:
                 if is_packable:
                     break
                     
-        sorted_items = sorted(items, key=lambda x: (-x[1], -x[0]))
+        sorted_items_with_indices = sorted(enumerate(items), key=lambda x: (-x[1][1], -x[1][0]))
+
         x_cursor, y_cursor = 0, 0
         max_row_height = 0
         
-        for w, h in sorted_items:
+        for i, (w, h) in sorted_items_with_indices:
             if x_cursor + w > self.container_width:
                 x_cursor = 0
                 y_cursor += max_row_height
                 max_row_height = 0
             
-            self.packing.append((x_cursor, y_cursor, w, h))
+            self.packing.append((i, x_cursor, y_cursor, w, h))
             x_cursor += w
             max_row_height = max(max_row_height, h)
 
     def construct_graphs(self):
-        for i, (x1, y1, w1, h1) in enumerate(self.packing):
+        for i, x1, y1, w1, h1 in self.packing:
             self.G_x.add_node(i, pos=(x1, y1), size=(w1, h1))
             self.G_y.add_node(i, pos=(x1, y1), size=(w1, h1))
 
             x1_end = x1 + w1
             y1_end = y1 + h1
             
-            for j, (x2, y2, w2, h2) in enumerate(self.packing):
+            for j, x2, y2, w2, h2 in self.packing:
                 if i == j:
                     continue
                 
@@ -82,92 +124,88 @@ class BinPackingGraph:
     def nx_to_pyg(self):
         num_nodes_x = self.G_x.number_of_nodes()
         x = torch.tensor(
-            [[self.G_x.nodes[i]['pos'][0], self.G_x.nodes[i]['pos'][1], self.G_x.nodes[i]['size'][0], self.G_x.nodes[i]['size'][1]] for i in range(num_nodes_x)], 
-            dtype=torch.float)
+            [[self.G_x.nodes[i]['pos'][0], self.G_x.nodes[i]['pos'][1], self.G_x.nodes[i]['size'][0], self.G_x.nodes[i]['size'][1]] 
+            for i in range(num_nodes_x)], dtype=torch.float)
 
         edge_index_x = torch.tensor(list(self.G_x.edges), dtype=torch.long).t().contiguous()
         edge_index_y = torch.tensor(list(self.G_y.edges), dtype=torch.long).t().contiguous()
 
-        return Data(x=x, edge_index_x=edge_index_x, edge_index_y=edge_index_y)
+        edge_index = torch.cat([edge_index_x, edge_index_y], dim=1) if edge_index_x.numel() and edge_index_y.numel() else edge_index_x
+
+        return Data(x=x, edge_index=edge_index, edge_index_x=edge_index_x, edge_index_y=edge_index_y)
 
 
-class GIN(torch.nn.Module):
-    def __init__(self, in_channels, hidden_channels, out_channels):
-        super(GIN, self).__init__()
-        self.conv1_x = GINConv(
-            nn.Sequential(
-                nn.Linear(in_channels, hidden_channels),
-                nn.BatchNorm1d(hidden_channels),
-                nn.ReLU()))
-        
-        self.conv2_x = GINConv(
-            nn.Sequential(
-                nn.Linear(hidden_channels, hidden_channels), 
-                nn.BatchNorm1d(hidden_channels),
-                nn.ReLU()))
-        
-        self.conv1_y = GINConv(
-            nn.Sequential(
-                nn.Linear(in_channels, hidden_channels),
-                nn.BatchNorm1d(hidden_channels),
-                nn.ReLU()))
-        
-        self.conv2_y = GINConv(
-            nn.Sequential(
-                nn.Linear(hidden_channels, hidden_channels), 
-                nn.BatchNorm1d(hidden_channels),
-                nn.ReLU()))
-        
-        self.lin1 = nn.Linear(hidden_channels*4, hidden_channels*2)
-        self.lin2 = nn.Linear(hidden_channels*2, out_channels)
+def attribute_masking(masked_x):
+    mask = torch.rand(data.x.shape, device=device) < mask_ratio
+    masked_x[mask] = 0
+    return masked_x
 
-    def forward(self, data):
-        x, edge_index_x, edge_index_y, batch = data.x, data.edge_index_x, data.edge_index_y, data.batch
-
-        h1_x = self.conv1_x(x, edge_index_x)
-        h2_x = self.conv2_x(h1_x, edge_index_x)
-
-        h1_y = self.conv1_y(x, edge_index_y)
-        h2_y = self.conv2_y(h1_y, edge_index_y)
-
-        h1_x = global_add_pool(h1_x, batch)
-        h2_x = global_add_pool(h2_x, batch)
-        h1_y = global_add_pool(h1_y, batch)
-        h2_y = global_add_pool(h2_y, batch)
-
-        h = torch.cat((h1_x, h2_x, h1_y, h2_y), dim=1)
-
-        h = self.lin1(h)
-        h = h.relu()
-        h = F.dropout(h, p=0.5, training=self.training)
-        h = self.lin2(h)
-        
-        return h, F.log_softmax(h, dim=1)
-    
 
 items_list = [
     [(3, 2), (5, 3), (2, 4), (4, 2), (3, 3)],
     [(2, 2), (3, 4), (4, 2), (1, 5), (3, 3)],
     [(8, 7), (7, 2), (6, 6), (9, 1), (7, 4)],
-    [(6, 1), (6, 4), (3, 5), (4, 8), (3, 7)],
+    [(3, 2), (5, 3), (2, 4), (4, 2), (3, 3)],
+    [(2, 2), (3, 4), (4, 2), (1, 5), (3, 3)],
+    [(8, 7), (7, 2), (6, 6), (9, 1), (7, 4)],
+    [(3, 2), (5, 3), (2, 4), (4, 2), (3, 3)],
+    [(2, 2), (3, 4), (4, 2), (1, 5), (3, 3)],
+    [(8, 7), (7, 2), (6, 6), (9, 1), (7, 4)],
+    [(3, 2), (5, 3), (2, 4), (4, 2), (3, 3)],
+    [(2, 2), (3, 4), (4, 2), (1, 5), (3, 3)],
+    [(8, 7), (7, 2), (6, 6), (9, 1), (7, 4)],
+    [(3, 2), (5, 3), (2, 4), (4, 2), (3, 3)],
+    [(2, 2), (3, 4), (4, 2), (1, 5), (3, 3)],
+    [(8, 7), (7, 2), (6, 6), (9, 1), (7, 4)],
+    [(3, 2), (5, 3), (2, 4), (4, 2), (3, 3)],
+    [(2, 2), (3, 4), (4, 2), (1, 5), (3, 3)],
+    [(8, 7), (7, 2), (6, 6), (9, 1), (7, 4)],
+    [(3, 2), (5, 3), (2, 4), (4, 2), (3, 3)],
+    [(2, 2), (3, 4), (4, 2), (1, 5), (3, 3)],
 ]
 
-container_width = 10
+container_size = (10, 10)
 
-dataset = []
-for items in items_list:
-    bp = BinPackingGraph(items, container_width)
-    data = bp.nx_to_pyg()
-    dataset.append(data)
+epochs = 1
+batch_size = 2
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-loader = DataLoader(dataset, batch_size=2, shuffle=True)
+dataset = [BinPackingGraph(items, container_size[0], container_size[1]).nx_to_pyg() for items in items_list]
+loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
-model = GIN(in_channels=4, hidden_channels=16, out_channels=8).to('cuda')
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-criterion = torch.nn.MSELoss()
+in_channels = 4
+hidden_dim = 16
+out_channels = 8
+mask_ratio = 0.3
 
-# for data in loader:
-#     data = data.to('cuda')
-#     print(data)
-#     embeddings = model(data)
-#     print(embeddings)
+model = BinPackingGAE(in_channels, hidden_dim, out_channels).to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.0003)
+
+for epoch in range(epochs):
+    model.train()
+    total_loss = 0
+    # loop = tqdm(loader, leave=True, ncols=100)
+
+    for data in loader:
+        data = data.to(device)
+
+        masked_x = attribute_masking(data.x.clone())
+
+        print(masked_x)
+        print(data.x)
+        break
+
+        # optimizer.zero_grad()
+        
+        # z = model.encode(data.x, data.edge_index)
+        # print(z.shape, data.edge_index.shape)
+
+        # loss = model.recon_loss(z, data.edge_index)
+        # loss.backward()
+        # optimizer.step()
+        
+        # total_loss += loss.item()
+    
+    # loop.postfix(epoch=epoch+1, loss=total_loss / len(loader))
+
+torch.save(model.state_dict(), "data/gae_bin_packing.pth")
