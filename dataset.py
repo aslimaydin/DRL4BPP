96610da5dccc656ebbacbcbaaf866ff9ca69989d
AAleka <mr.allnur.55@gmail.com>
2025-03-26 09:46:38 +0300

    dataset processing

--- dataset.py
+++ dataset.py
@@ -1,152 +1,296 @@
-import shutil
 import torch
-import os
-import h5py
-import numpy as np
-from torch.utils.data import Dataset, DataLoader
-from tqdm import tqdm
-from concurrent.futures import ProcessPoolExecutor
+import networkx as nx
+import matplotlib.pyplot as plt
+
+from torch.utils.data import Dataset
+from torch_geometric.data import Data
+from torch_geometric.loader import DataLoader
+from random import shuffle
+from matplotlib.patches import Rectangle
 
 
-def generate_single_set_h5py(set_id, range_weights, range_values):
-    n_weights = np.random.randint(range_weights[0], range_weights[1])
-    weights = np.sort(np.random.uniform(low=range_values[0], high=range_values[1], size=n_weights)).astype(np.float32)
+class BinPacking2D(Dataset):
+    def __init__(self, file_name):
+        self.labels = []
+        self.items = []
+        self.packing = []
+        self.process_data(file_name)
 
-    adjacency_list = []
-    for i in range(n_weights):
-        for j in range(i + 1, n_weights):
-            normalized_sum = weights[i] + weights[j]
-            if normalized_sum <= 1.0 and i != j:
-                adjacency_list.append([i, j, normalized_sum])
-                adjacency_list.append([j, i, normalized_sum])
+    def process_data(self, file_name):
+        with open(file_name, 'r') as f:
+            group = []
+            items = []
+            bin_size = None
 
-    return set_id, weights, np.array(adjacency_list, dtype=np.float32)
+            for line in f:
+                stripped = line.strip()
+                if stripped:
+                    values = list(map(float, stripped.split()))
+                    if len(values) == 2:
+                        bin_size = values
+                    else:
+                        if bin_size is None:
+                            raise ValueError("Bin size must be defined before normalizing values.")
 
+                        normalized_values = [
+                            values[0] / bin_size[0],
+                            values[1] / bin_size[1],
+                            values[2] / bin_size[0],
+                            values[3] / bin_size[1]
+                        ]
+                        group.append(normalized_values)
+                        items.append(normalized_values[:2])
+                else:
+                    if group:
+                        self.labels.append(group)
+                        self.items.append(items)
+                        self.packing.append(group)
+                        group = []
+                        items = []
 
-def generate_data_1D(data_dir="data", num_workers=os.cpu_count(), n_sets=1000, range_weights=(100, 500), range_values=(0.01, 0.99)):
-    if os.path.exists(data_dir):
-        shutil.rmtree(data_dir)
-    
-    os.mkdir(data_dir)
+            if group:
+                self.labels.append(group)
+                self.items.append(items)
+                self.packing.append(group)
 
-    with h5py.File(os.path.join(data_dir, "train.h5"), "w") as h5file:
-        grp_weights = h5file.create_group("weights")
-        grp_adj = h5file.create_group("adjacency")
+    def construct_graphs(self, packing):
+        G_x = nx.Graph()
+        G_y = nx.Graph()
 
-        print("Generating and saving data...")
-        with ProcessPoolExecutor(max_workers=num_workers) as executor:
-            for set_id, weights, adjacency in tqdm(
-                executor.map(generate_single_set_h5py, 
-                            range(n_sets), 
-                            [range_weights] * n_sets, 
-                            [range_values] * n_sets), total=n_sets, ncols=100):
+        for i, (x1, y1, w1, h1) in enumerate(packing):
+            G_x.add_node(i, pos=(x1, y1), size=(w1, h1))
+            G_y.add_node(i, pos=(x1, y1), size=(w1, h1))
 
-                grp_weights.create_dataset(str(set_id), data=weights)
-                grp_adj.create_dataset(str(set_id), data=adjacency)
-    
-    n_sets = n_sets // 10
+            x1_end = x1 + w1
+            y1_end = y1 + h1
 
-    with h5py.File(os.path.join(data_dir, "test.h5"), "w") as h5file:
-        grp_weights = h5file.create_group("weights")
-        grp_adj = h5file.create_group("adjacency")
+            for j, (x2, y2, w2, h2) in enumerate(packing):
+                if i == j:
+                    continue
+                
+                x2_end = x2 + w2
+                y2_end = y2 + h2
 
-        print("Generating and saving data...")
-        with ProcessPoolExecutor(max_workers=num_workers) as executor:
-            for set_id, weights, adjacency in tqdm(
-                executor.map(generate_single_set_h5py, 
-                            range(n_sets), 
-                            [range_weights] * n_sets, 
-                            [range_values] * n_sets), total=n_sets, ncols=100):
+                if not (x1_end <= x2 or x2_end <= x1):
+                    G_y.add_edge(i, j)
+                
+                if not (y1_end <= y2 or y2_end <= y1):
+                    G_x.add_edge(i, j)
 
-                grp_weights.create_dataset(str(set_id), data=weights)
-                grp_adj.create_dataset(str(set_id), data=adjacency)
+        return G_x, G_y
 
+    def nx_to_pyg(self, G_x, G_y):
+        num_nodes = G_x.number_of_nodes()
+        x = torch.tensor(
+            [[G_x.nodes[i]['pos'][0], G_x.nodes[i]['pos'][1], G_x.nodes[i]['size'][0], G_x.nodes[i]['size'][1]] for i in range(num_nodes)], 
+            dtype=torch.float
+        )
 
-class BinPackingDataset(Dataset):
-    def __init__(self, filename=os.path.join("data", "train.h5")):
-        self.h5 = h5py.File(filename, "r")
-        self.instances = list(self.h5["weights"].keys())
+        edge_index_x = torch.tensor(list(G_x.edges), dtype=torch.long).t().contiguous()
+        edge_index_y = torch.tensor(list(G_y.edges), dtype=torch.long).t().contiguous()
+
+        return Data(x=x, edge_index_x=edge_index_x, edge_index_y=edge_index_y)
+
+    def random_packing(self, items):
+        packing = []
+        shuffled_items = items.copy()
+        shuffle(shuffled_items)
+        x_cursor, y_cursor = 0, 0
+        max_row_height = 0
+
+        for w, h in shuffled_items:
+            if x_cursor + w > 1.0:
+                x_cursor = 0
+                y_cursor += max_row_height
+                max_row_height = 0
+            
+            packing.append((w, h, x_cursor, y_cursor))
+            x_cursor += w
+            max_row_height = max(max_row_height, h)
+
+        return packing
     
     def __len__(self):
-        return len(self.instances)
-    
-    def load_sparse_adj(self, instance_id, n_items):
-        if instance_id not in self.h5["adjacency"]:
-            return torch.sparse_coo_tensor(([], []), [], (n_items, n_items))
+        return len(self.items)
 
-        adjacency_data = self.h5["adjacency"][instance_id][:]
-        
-        if adjacency_data.size == 0:
-            return torch.sparse_coo_tensor(([], []), [], (n_items, n_items))
-
-        indices_list = adjacency_data[:, :2].astype(np.int64)
-        values_list = adjacency_data[:, 2]
-
-        indices = torch.tensor(indices_list.T, dtype=torch.long)
-        values = torch.tensor(values_list, dtype=torch.float32)
-
-        return torch.sparse_coo_tensor(indices, values, (n_items, n_items))
-    
     def __getitem__(self, idx):
-        instance_id = self.instances[idx]
-        
-        weights = self.h5["weights"][instance_id][:]
-        weights_tensor = torch.tensor(weights, dtype=torch.float32)
-        n_items = len(weights)
-        
-        adj_matrix = self.load_sparse_adj(instance_id, n_items)
+        label_packing = self.packing[idx]
+        G_x, G_y = self.construct_graphs(label_packing)
 
-        return weights_tensor, adj_matrix, n_items
-    
-    def close(self):
-        self.h5.close()
+        random_packing = self.random_packing(self.items[idx])
+        random_G_x, random_G_y = self.construct_graphs(random_packing)
+        
+        return self.nx_to_pyg(G_x, G_y), self.nx_to_pyg(random_G_x, random_G_y)
 
 
-def collate_binpacking(batch):
-    max_items = max(items for _, _, items in batch)
-    
-    weights_batch = []
-    adj_batch = []
-    mask_batch = []
-    
-    for weights, adj, n_items in batch:
-        padded_weights = torch.zeros(max_items, dtype=torch.float32)
-        padded_weights[:n_items] = weights
-        weights_batch.append(padded_weights)
+class BinPackingGraph:
+    def __init__(self, items, container_width):
+        self.packing = []
+        self.container_width = container_width
         
-        padded_adj = torch.zeros((max_items, max_items), dtype=torch.float32)
-        padded_adj[:n_items, :n_items] = adj.to_dense()
-        adj_batch.append(padded_adj)
+        self.G_x = nx.Graph()
+        self.G_y = nx.Graph()
+
+        self.bottom_left_packing(items)
+        self.construct_graphs()
         
-        mask = torch.zeros(max_items, dtype=torch.bool)
-        mask[:n_items] = True
-        mask_batch.append(mask)
-    
-    weights_batch = torch.stack(weights_batch)
-    adj_batch = torch.stack(adj_batch)
-    mask_batch = torch.stack(mask_batch)
-    
-    return weights_batch.unsqueeze(-1), adj_batch, mask_batch
+    def bottom_left_packing(self, items):
+        is_packable = False
+        for i, (w1, _) in enumerate(items):
+            for j, (w2, _) in enumerate(items):
+                if i == j:
+                    continue
+
+                if w1 + w2 <= self.container_width:
+                    is_packable = True
+                    break
+                
+        if not is_packable:
+            for i, (_, h1) in enumerate(items):
+                for j, (w2, _) in enumerate(items):
+                    if i == j:
+                        continue
+
+                    if h1 + w2 <= self.container_width:
+                        items[i] = (items[i][1], items[i][0])
+                        is_packable = True
+                        break
+                
+                if is_packable:
+                    break
+
+        sorted_items_with_indices = sorted(enumerate(items), key=lambda x: (-x[1][1], -x[1][0]))
+        x_cursor, y_cursor = 0, 0
+        max_row_height = 0
+        
+        for i, (w, h) in sorted_items_with_indices:
+            if x_cursor + w > self.container_width:
+                x_cursor = 0
+                y_cursor += max_row_height
+                max_row_height = 0
+            
+            self.packing.append((i, x_cursor, y_cursor, w, h))
+            x_cursor += w
+            max_row_height = max(max_row_height, h)
+
+    def random_packing(self, items):
+        is_packable = False
+        for i, (w1, _) in enumerate(items):
+            for j, (w2, _) in enumerate(items):
+                if i == j:
+                    continue
+
+                if w1 + w2 <= self.container_width:
+                    is_packable = True
+                    break
+                
+        if not is_packable:
+            for i, (_, h1) in enumerate(items):
+                for j, (w2, _) in enumerate(items):
+                    if i == j:
+                        continue
+
+                    if h1 + w2 <= self.container_width:
+                        items[i] = (items[i][1], items[i][0])
+                        is_packable = True
+                        break
+                
+                if is_packable:
+                    break
+        
+        shuffled_items = items.copy()
+        shuffle(shuffled_items)
+        x_cursor, y_cursor = 0, 0
+        max_row_height = 0
+
+        for w, h in shuffled_items:
+            if x_cursor + w > self.container_width:
+                x_cursor = 0
+                y_cursor += max_row_height
+                max_row_height = 0
+            
+            self.packing.append((x_cursor, y_cursor, w, h))
+            x_cursor += w
+            max_row_height = max(max_row_height, h)
+        
+    def construct_graphs(self):
+        for i, x1, y1, w1, h1 in self.packing:
+            self.G_x.add_node(i, pos=(x1, y1), size=(w1, h1))
+            self.G_y.add_node(i, pos=(x1, y1), size=(w1, h1))
+
+            x1_end = x1 + w1
+            y1_end = y1 + h1
+            
+            for j, x2, y2, w2, h2 in self.packing:
+                if i == j:
+                    continue
+                
+                x2_end = x2 + w2
+                y2_end = y2 + h2
+
+                if not (x1_end <= x2 or x2_end <= x1):
+                    self.G_y.add_edge(i, j)
+                
+                if not (y1_end <= y2 or y2_end <= y1):
+                    self.G_x.add_edge(i, j)
+
+    def nx_to_pyg(self):
+        num_nodes_x = self.G_x.number_of_nodes()
+        x = torch.tensor(
+            [[self.G_x.nodes[i]['pos'][0], self.G_x.nodes[i]['pos'][1], self.G_x.nodes[i]['size'][0], self.G_x.nodes[i]['size'][1]] for i in range(num_nodes_x)], 
+            dtype=torch.float)
+
+        edge_index_x = torch.tensor(list(self.G_x.edges), dtype=torch.long).t().contiguous()
+        edge_index_y = torch.tensor(list(self.G_y.edges), dtype=torch.long).t().contiguous()
+
+        return Data(x=x, edge_index_x=edge_index_x, edge_index_y=edge_index_y)
+
+    def plot_packing(self):
+        fig, ax = plt.subplots(figsize=(12, 6))
+
+        for i, x, y, w, h in self.packing:
+            rect = Rectangle((x, y), w, h, linewidth=1, edgecolor='black', facecolor='lightblue', alpha=0.7)
+            ax.add_patch(rect)
+
+            if x == int(x): x = int(x)
+            if y == int(y): y = int(y)
+            if w == int(w): w = int(w)
+            if h == int(h): h = int(h)
+            ax.text(x + w / 2, y + h / 2, f'({w}, {h})', fontsize=6, ha='center', va='center', color='black')
+
+        ax.set_xlim(0, max(x + w for _, x, _, w, _ in self.packing) + 1)
+        ax.set_ylim(0, max(y + h for _, _, y, _, h in self.packing) + 1)
+        ax.set_xticks([])
+        ax.set_yticks([])
+        ax.set_title("Packed Rectangles")
+        ax.set_aspect('equal')
+
+        fig.set_size_inches(18.5, 10.5)
+        plt.show()
+        plt.close()
+
+    def plot_graphs(self):
+        fig = plt.figure(figsize=(12, 6))
+        plt.subplot(1, 2, 1)
+        nx.draw(self.G_x, with_labels=True)
+        plt.title("G_x")
+
+        plt.axvline(x = 5, color = 'b')
+        
+        plt.subplot(1, 2, 2)
+        nx.draw(self.G_y, with_labels=True)
+        plt.title("G_y")
+
+        fig.set_size_inches(18.5, 10.5)
+        plt.show()
+        plt.close()
 
 
 if __name__ == "__main__":
-    generate_data_1D(n_sets=1000, range_weights=(100, 200))
+    dataset = BinPacking2D("data/gcut/combined1.txt")
+    loader = DataLoader(dataset, batch_size=1, shuffle=True)
 
-    dataset = BinPackingDataset("data")
-    loader = DataLoader(
-        dataset,
-        batch_size=1,
-        shuffle=True,
-        num_workers=8,
-        collate_fn=collate_binpacking,
-        pin_memory=torch.cuda.is_available()
-    )
-
-    for weights, adj, mask in loader:
-        print(weights)
-        print(adj)
-        break
-        # print(f"Weights: {weights.shape}")
-        # print(f"Adjacency: {adj.shape}")
-        # print(f"Mask: {mask.shape}")
-        # print(f"Valid items per instance: {mask.sum(dim=1)}")
+    for labels, inputs in loader:
+        print(labels.x)
+        print(inputs.x)
+        print()
--- model.py
+++ model.py
@@ -1,158 +1,101 @@
-import random
 import torch
-import torch.nn as nn
 import torch.nn.functional as F
+from torch import nn
+from torch_geometric.nn import GINConv, global_add_pool
 
 
-def save_checkpoint(model, optimizer, filename="my_checkpoint.pth.tar"):
-    checkpoint = {
-        "state_dict": model.state_dict(),
-        "optimizer": optimizer.state_dict(),
-    }
-    torch.save(checkpoint, filename)
-
-
-def load_checkpoint(checkpoint_file, model, optimizer, lr):
-    print("=> Loading checkpoint")
-    checkpoint = torch.load(checkpoint_file, map_location=torch.device("cuda" if torch.cuda.is_available() else "cpu"))
-    model.load_state_dict(checkpoint["state_dict"])
-
-    if optimizer is not None:
-        optimizer.load_state_dict(checkpoint["optimizer"])
-
-        for param_group in optimizer.param_groups:
-            param_group["lr"] = lr
-
-
-class GATEncoder(nn.Module):
-    def __init__(self, input_dim=1, hidden_dim=32, embedding_dim=16, n_layers=1, n_heads=4, dropout=0.1):
-        super(GATEncoder, self).__init__()
-        self.n_layers = n_layers
-        self.embedding_dim = embedding_dim
+class GIN(nn.Module):
+    def __init__(self, in_channels, hidden_channels, out_channels):
+        super(GIN, self).__init__()
+        self.conv1_x = GINConv(
+            nn.Sequential(
+                nn.Linear(in_channels, hidden_channels),
+                nn.BatchNorm1d(hidden_channels),
+                nn.ReLU(),
+                nn.Linear(hidden_channels, hidden_channels),
+                nn.ReLU()))
         
-        self.input_proj = nn.Linear(input_dim, hidden_dim)
+        self.conv1_y = GINConv(
+            nn.Sequential(
+                nn.Linear(in_channels, hidden_channels),
+                nn.BatchNorm1d(hidden_channels),
+                nn.ReLU(),
+                nn.Linear(hidden_channels, hidden_channels),
+                nn.ReLU()))
         
-        self.gat = nn.ModuleList([])
+        self.conv2_x = GINConv(
+            nn.Sequential(
+                nn.Linear(hidden_channels, hidden_channels), 
+                nn.BatchNorm1d(hidden_channels),
+                nn.ReLU(),
+                nn.Linear(hidden_channels, hidden_channels),
+                nn.ReLU()))
+        
+        self.conv2_y = GINConv(
+            nn.Sequential(
+                nn.Linear(hidden_channels, hidden_channels), 
+                nn.BatchNorm1d(hidden_channels),
+                nn.ReLU(),
+                nn.Linear(hidden_channels, hidden_channels),
+                nn.ReLU()))
+        
+        self.conv3_x = GINConv(
+            nn.Sequential(
+                nn.Linear(hidden_channels, hidden_channels),
+                nn.BatchNorm1d(hidden_channels),
+                nn.ReLU(),
+                nn.Linear(hidden_channels, hidden_channels), 
+                nn.ReLU()))
+        
+        self.conv3_y = GINConv(
+            nn.Sequential(
+                nn.Linear(hidden_channels, hidden_channels),
+                nn.BatchNorm1d(hidden_channels),
+                nn.ReLU(),
+                nn.Linear(hidden_channels, hidden_channels), 
+                nn.ReLU()))
+        
+        self.lin1 = nn.Linear(hidden_channels*3*2, hidden_channels*3)
+        self.lin2 = nn.Linear(hidden_channels*3, out_channels)
 
-        for i in range(n_layers):
-            mult = n_heads if i > 0 else 1
-            self.gat.append(
-                nn.ModuleList([
-                    GraphAttentionLayer(hidden_dim * mult, hidden_dim, dropout) 
-                    for _ in range(n_heads)
-                ])
-            )
-        
-        self.proj = nn.Linear(hidden_dim * n_heads, embedding_dim)
-        
-        self.activation = nn.ELU()
-        self.dropout = nn.Dropout(dropout)
-        
-    def forward(self, x):
-        h = self.input_proj(x)
-        
-        batch_size, N, _ = h.size()
-        adj = torch.ones(batch_size, N, N).to(x.device)
-        
-        for layer in self.gat:
-            h = torch.cat([att(h, adj) for att in layer], dim=-1)
-            h = self.dropout(h)
+    def forward(self, data):
+        x, edge_index_x, edge_index_y, batch = data.x, data.edge_index_x, data.edge_index_y, data.batch
+        print(x.shape, edge_index_x.shape, edge_index_y.shape)
+        if edge_index_x.numel() == 0:
+            h1_x = x.clone()
+            h2_x = x.clone()
+            h3_x = x.clone()
+        else:
+            h1_x = self.conv1_x(x, edge_index_x)
+            h2_x = self.conv2_x(h1_x, edge_index_x)
+            h3_x = self.conv3_x(h2_x, edge_index_x)
 
-        embeddings = self.proj(h)
+        if edge_index_y.numel() == 0:
+            h1_y = x.clone()
+            h2_y = x.clone()
+            h3_y = x.clone()
+        else:
+            h1_y = self.conv1_y(x, edge_index_y)
+            h2_y = self.conv2_y(h1_y, edge_index_y)
+            h3_y = self.conv3_y(h2_y, edge_index_y)
+
+        h1_x = global_add_pool(h1_x, batch)
+        h2_x = global_add_pool(h2_x, batch)
+        h3_x = global_add_pool(h3_x, batch)
+
+        h1_y = global_add_pool(h1_y, batch)
+        h2_y = global_add_pool(h2_y, batch)
+        h3_y = global_add_pool(h3_y, batch)
+
+        h = torch.cat((h1_x, h2_x, h3_x, h1_y, h2_y, h3_y), dim=1)
+
+        h = self.lin1(h)
+        h = h.relu()
+        h = F.dropout(h, p=0.5, training=self.training)
+        h = self.lin2(h)
         
-        return embeddings
+        return h, F.log_softmax(h, dim=1)
+
+
+# if __name__ == "__main__":
     
-    
-class GraphAttentionLayer(nn.Module):
-    def __init__(self, in_features, out_features, dropout=0.1):
-        super(GraphAttentionLayer, self).__init__()
-        
-        self.in_features = in_features
-        self.out_features = out_features
-        self.dropout = dropout
-        
-        self.W = nn.Linear(in_features, out_features, bias=False)
-        self.a = nn.Linear(2 * out_features, 1, bias=False)
-        
-        self.leakyrelu = nn.LeakyReLU(0.2)
-        
-    def forward(self, h, adj):
-        Wh = self.W(h)
-        
-        N = Wh.size(1)
-        
-        a_input = torch.cat([Wh.repeat_interleave(N, dim=1), 
-                           Wh.repeat(1, N, 1)], dim=2)
-        e = self.leakyrelu(self.a(a_input)).squeeze(2)
-        e = e.view(-1, N, N)
-        
-        zero_vec = -9e15 * torch.ones_like(e)
-        attention = torch.where(adj > 0, e, zero_vec)
-        attention = F.softmax(attention, dim=2)
-        attention = F.dropout(attention, self.dropout, training=self.training)
-        
-        h_prime = torch.bmm(attention, Wh)
-        
-        return h_prime
-    
-
-class Decoder(nn.Module):
-    def __init__(self, embedding_dim=16, hidden_dim=32):
-        super(Decoder, self).__init__()
-        
-        self.hidden = nn.Linear(2 * embedding_dim, hidden_dim)
-        self.output = nn.Linear(hidden_dim, 1)
-        
-    def forward(self, embeddings):
-        batch_size, N, _ = embeddings.size()
-        
-        emb1 = embeddings.repeat_interleave(N, dim=1)
-        emb2 = embeddings.repeat(1, N, 1)
-        
-        pairs = torch.cat([emb1, emb2], dim=2)
-        
-        h = F.relu(self.hidden(pairs))
-        adj_pred = torch.sigmoid(self.output(h))
-        
-        return adj_pred.view(batch_size, N, N)
-
-class GATAutoencoder(nn.Module):
-    def __init__(self, input_dim=1, hidden_dim=32, embedding_dim=16, n_layers=1, n_heads=4, dropout=0.1):
-        super(GATAutoencoder, self).__init__()
-        
-        self.encoder = GATEncoder(input_dim, hidden_dim, embedding_dim, n_layers, n_heads, dropout)
-        self.decoder = Decoder(embedding_dim, hidden_dim)
-        
-    def forward(self, x):
-        embeddings = self.encoder(x)
-        
-        adj_pred = self.decoder(embeddings)
-        
-        return embeddings, adj_pred
-    
-
-if __name__ == "__main__":
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    
-    min_weights = 100
-    max_weights = 200
-
-    batch_size = 1
-    
-    model = GATAutoencoder(input_dim=1, hidden_dim=16, embedding_dim=8, n_layers=3, n_heads=4, dropout=0.1).to(device)
-    print(model)
-    num_weights = random.randint(min_weights, max_weights)
-    weights, _ = torch.sort(torch.rand(batch_size, num_weights, 1, dtype=torch.float32, device=device), dim=1)
-    adj_matrix = torch.zeros((batch_size, num_weights, num_weights), dtype=torch.float32).to(device)
-    
-    print("Data generation...")
-    for batch in range(batch_size):
-        for i in range(num_weights):
-            for j in range(num_weights):
-                normalized_sum = weights[batch, i] + weights[batch, j]
-                if normalized_sum <= 1.0 and i != j:
-                    adj_matrix[batch, i, j] = normalized_sum
-
-    print(weights.shape, adj_matrix.shape)
-    
-    embeddings, adj_matrix_pred = model(weights)
